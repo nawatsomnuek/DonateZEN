6276b0c226bf230c0e0653411295b08b9341373b
siwa <siwa1909@gmail.com>
2018-02-21 21:25:08 +0700

    edit ConnectionBuilder.java

--- src/main/java/model/ConnectionBuilder.java
+++ src/main/java/model/ConnectionBuilder.java
@@ -24,20 +24,20 @@ public class ConnectionBuilder {
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
 
         try {
-//            Properties prop = new Properties();
-//            InputStream input = classloader.getResourceAsStream("config.properties");
-//            prop.load(input);
-//            String url = prop.getProperty("DONATEZEN_URL");
-//            String port = prop.getProperty("DONATEZEN_PORT");
-//            String dbname = prop.getProperty("DONATEZEN_DBNAME");
-//            String username = prop.getProperty("DONATEZEN_USR");
-//            String password = prop.getProperty("DONATEZEN_PAS");
+            Properties prop = new Properties();
+            InputStream input = classloader.getResourceAsStream("config.properties");
+            prop.load(input);
+            String url = prop.getProperty("DONATEZEN_URL");
+            String port = prop.getProperty("DONATEZEN_PORT");
+            String dbname = prop.getProperty("DONATEZEN_DBNAME");
+            String username = prop.getProperty("DONATEZEN_USR");
+            String password = prop.getProperty("DONATEZEN_PAS");
 
-            String url = "donatezen.cjbwyadvduih.ap-southeast-1.rds.amazonaws.com";
-            String port = "3306";
-            String dbname =  "donatezen";
-            String username =  "admin";
-            String password = "Qwertyui123";
+//            String url = "donatezen.cjbwyadvduih.ap-southeast-1.rds.amazonaws.com";
+//            String port = "3306";
+//            String dbname =  "donatezen";
+//            String username =  "admin";
+//            String password = "Qwertyui123";
 
             Class.forName("com.mysql.jdbc.Driver");
             con = DriverManager.getConnection("jdbc:mysql://" + url + ":" + port + "/" + dbname + "?user=" + username + "&password=" + password);
